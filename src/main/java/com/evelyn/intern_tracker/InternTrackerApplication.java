266f327602e225f255057118fcb1c561a76d74b6
Evelyn Sun <evelynsun632@gmail.com>
2025-11-25 22:54:17 -0500

    Initial Spring Boot backend setup

---
+++ src/main/java/com/evelyn/intern_tracker/InternTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.evelyn.intern_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evelyn/intern_tracker/InternTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evelyn.intern_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
